02271027e7d68dcc97417a5d9b27bf8e9f9e1473
Julien Loizelet <julienloizelet@okaeli.com>
2024-10-18 14:32:56 +0900

    feat(lapi): Handle max body size for AppSec (#29)

--- tests/Integration/CapiRemediationTest.php
+++ tests/Integration/CapiRemediationTest.php
@@ -125,7 +125,7 @@ final class CapiRemediationTest extends TestCase
         // Empty log file
         file_put_contents($this->root->url() . '/' . $this->debugFile, '');
         $result = $remediationEngine->refreshDecisions();
-        $this->assertTrue(in_array((int) $result['new'], [0,1]));
+        $this->assertTrue(in_array((int) $result['new'], [0, 1]));
 
         PHPUnitUtil::assertRegExp(
             $this,
